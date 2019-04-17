a24a74a1a18e34d48dcd906591efe972b705caaf
Yulong Niu <yulong.niu@hotmail.com>
2019-04-17 17:44:46 +0200

    add all gene plot

--- scripts/script_hieracluster.R
+++ scripts/script_hieracluster.R
@@ -85,6 +85,18 @@ hr %>%
        ylab = 'Height')
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+##~~~~~~~~~~~~~~~~~~~~~~~~~~dynamic tree cut~~~~~~~~~~~~~~~~~~~~
+clusDyn <- scaleCount %>%
+  t %>%
+  cor %>%
+  {1 - .} %>%
+  as.dist %T>%
+  gc %>%
+  as.matrix %T>%
+  gc %>%
+  cutreeDynamic(hr, distM = ., method = 'hybrid')
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 ##~~~~~~~~~~~~~~~~~~~~~~~~cut trees by height ~~~~~~~~~~~~~~~~~~~~~~~~~
 hclusth1.5 <- cutree(hr, h = 1.5)
 hclusth1.0 <- cutree(hr, h = 1.0)
@@ -99,11 +111,11 @@ plot(treeR,
      main = 'Gene Clustering',
      ylab = 'Height')
 
-cbind(hclusth0.5, hclusth1.0, hclusth1.5) %>%
+cbind(hclusth0.5, hclusth1.0, hclusth1.5, clusDyn) %>%
   colored_bars(treeR,
                sort_by_labels_order = TRUE,
                y_shift = -0.1,
-               rowLabels = c('h=0.5','h=1.0','h=1.5'),
+               rowLabels = c('h=0.5','h=1.0','h=1.5', 'Dynamic'),
                cex.rowLabels=0.7)
 
 abline(h=1.5, lty = 2, col='grey')
@@ -115,44 +127,47 @@ cgenes <- c('AT1G14550.1', 'AT2G30750.1', 'AT2G19190.1')
 hclusth1.5[cgenes]
 hclusth1.0[cgenes]
 hclusth0.5[cgenes]
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-##~~~~~~~~~~~~~~~~~~~~~~~~~~dynamic tree cut~~~~~~~~~~~~~~~~~~~~
-clusDyn <- scaleCount %>%
-  t %>%
-  cor %>%
-  {1 - .} %>%
-  as.dist
-
-cutreeDynamic(hr, clusDyn, method = 'hybrid')
-clusDyn <- cutreeDynamic(hr, distM = as.matrix(as.dist(1-cor(t(scaledata)))), method = "hybrid")
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~plot patterns~~~~~~~~~~~~~~~~~~~~~~~~
-## plot cores
-clusterCore <- scaleCount %>%
+## join cluster and scaled normalized counts
+clusterGene <- scaleCount %>%
   as.data.frame %>%
   rownames_to_column(var = 'ID') %>%
   as_tibble %>%
   {
-    cl <- as.data.frame(hclusth0.5) %>%
+    cl <- as.data.frame(hclusth1.5) %>%
       rownames_to_column(var = 'ID')
     inner_join(., cl)
-  } %>% ## join cluster and scaled normalized counts
-  group_by(hclusth0.5) %>%
+  }
+
+clusterCore <- clusterGene %>%
+  group_by(hclusth1.5) %>%
   summarise_at(2:5, mean, na.rm = TRUE) %>% ## mean of each cluster
-  mutate(hclusth0.5 = hclusth0.5 %>% paste0('cluster_', .)) %>%
+  mutate(hclusth1.5 = hclusth1.5 %>% paste0('cluster_', .)) %>%
   gather(Sample, NorExpress, Mock : flg22_SynCom35)
 
 clusterCore$Sample %<>% factor(levels = c('Mock', 'flg22', 'flg22_SynCom33', 'flg22_SynCom35'), ordered = TRUE)
 
-ggplot(clusterCore, aes(Sample, NorExpress, col = hclusth0.5, group = hclusth0.5)) +
+## plot core cluster
+ggplot(clusterCore, aes(Sample, NorExpress, col = hclusth1.5, group = hclusth1.5)) +
   geom_point() +
   geom_line() +
   ylab('Scaled counts') +
-  facet_wrap(. ~ hclusth0.5, ncol = 5) +
+  facet_wrap(. ~ hclusth1.5, ncol = 2) +
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
-ggsave('hieracluster_0d5.pdf')
+ggsave('hieracluster_1d5.pdf')
+
+clusterGenePlot <- clusterGene %>%
+  gather(Sample, NorExpress, Mock : flg22_SynCom35) %>%
+  mutate(hclusth1.5 = hclusth1.5 %>% paste0('cluster_', .))
+
+ggplot(clusterGenePlot, aes(Sample, NorExpress, group = ID)) +
+  geom_line(color = 'grey', alpha = 0.1) +
+  facet_wrap(. ~ hclusth1.5, ncol = 2)
+
+ggplot()
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #################################################################
