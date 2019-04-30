88b7c7bc655b101b431ad8e27aed1012efee4954
Yulong Niu <yulong.niu@hotmail.com>
2019-04-30 05:24:00 +0200

    add geneset plot

--- scripts/script_path.R
+++ scripts/script_path.R
@@ -60,13 +60,11 @@ BioCycMat <- foreach(i = 1:length(athBioCyc), .combine = rbind) %dopar% {
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~choose sig~~~~~~~~~~~~~~~~~~~~~
 ## padj < 0.05 & |log2FC| > log2(1.5)
-
 fcsig <- kmeansRes %>%
   select(ends_with('FoldChange')) %>%
   transmute_all(list(~ case_when(. > 1 ~ 1,
                                  . < -1 ~ -1,
                                  TRUE ~ 0)))
-
 padjsig <- kmeansRes %>%
   select(ends_with('padj')) %>%
   abs %>%
@@ -100,7 +98,8 @@ for (i in vsGroup) {
 
     GOTestWithCat <- goseq(pwf, gene2cat = GOMat, use_genes_without_cat = FALSE) %>%
       as_tibble %>%
-      filter(!is.na(ontology))
+      filter(!is.na(ontology)) %>%
+      rename(Annotation = term)
 
     termCat <- c('BP', 'MF', 'CC')
     for (k in termCat) {
@@ -165,5 +164,49 @@ for (i in vsGroup) {
   }
 }
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 ##############################################################
+
+
+###################################plot###########################
+setwd('/extDisk1/RESEARCH/MPIPZ_KaWai_RNASeq/results/pathway')
+
+library('ggplot2')
+library('readr')
+library('dplyr')
+library('magrittr')
+library('foreach')
+
+
+cln <- 1:10
+geneset <- c('BP', 'MF', 'CC', 'KEGG', 'BioCyc')
+
+for (i in cln) {
+  for (j in geneset) {
+
+    vsGroup <- c('Flg22_vs_Mock', 'Flg22_SynCom33_vs_Mock', 'Flg22_SynCom35_vs_Mock')
+
+    pathPlot <- foreach(k = seq_along(vsGroup), .combine = bind_rows) %do% {
+      vsGroup[k] %>%
+        {paste0('kmeans10_', ., '_cluster', i, '_', j, '.csv')} %>%
+        read_csv %>%
+        select(Annotation, over_represented_pvalue, numDEInCat, numInCat) %>%
+        rename(pvalue = over_represented_pvalue) %>%
+        mutate(group = vsGroup[k], ratio = numDEInCat / numInCat) %>%
+        filter(pvalue < 0.05 &
+               numDEInCat > 3)
+    }
+
+    colorPal <- colorRampPalette(rev(c('red', 'yellow', 'cyan', 'blue')), bias=1)(10)
+
+    ggplot(pathPlot, aes(x = group, y = Annotation)) +
+      geom_point(aes(size = ratio, colour = -log10(pvalue))) +
+      scale_colour_gradientn(name = '-log10(P-value)', limits=c(0, max(-log10(pathPlot$pvalue))), colours = colorPal) +
+      ## scale_x_discrete(labels = c('Flg22', 'Flg22+SynCom33', 'Flg22+SynCom35')) +
+      ylab(j) +
+      xlab('') +
+      theme(axis.text.x = element_text(angle = 90, hjust = 1))
+    ggsave(paste0('kmeans10_cluster', i, '_', j, '.pdf'))
+    ggsave(paste0('kmeans10_cluster', i, '_', j, '.jpg'))
+  }
+}
+##################################################################
