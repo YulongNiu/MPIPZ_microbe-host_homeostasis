99b0e9d4fe1b794dcb9a24bb3dfebe519cfee5bc
Yulong Niu <yulong.niu@hotmail.com>
2019-10-13 18:42:50 +0200

    fix R1 R2 unequal

--- scripts/script_alignment.sh
+++ scripts/script_alignment.sh
@@ -2,7 +2,7 @@ date
 
 REF_PATH=/netscratch/dep_psl/grp_rgo/yniu/ref/ath
 CLEAN_PATH=/netscratch/dep_psl/grp_rgo/yniu/KaWaiFlg22/clean_data_soil
-ALIGN_PATH=/netscratch/dep_psl/grp_rgo/yniu/KaWaiFlg22/align_data_soil
+ALIGN_PATH=/netscratch/dep_psl/grp_rgo/yniu/KaWaiFlg22/align_data_soil_virus1
 
 KALLISTO_PATH=/home/yniu/Biotools/kallisto_v0.45.1
 HISAT2_PATH=/home/yniu/Biotools/hisat2-2.1.0
@@ -75,6 +75,40 @@ for i in ${fqnames[@]}; do
     echo "HISAT2 using ${SPECIES} genome for ${i}."
     ${HISAT2_PATH}/hisat2 -p ${CORENUM} \
                   -x ${REF_PATH}/athht2index/genome \
+                  -1 ${CLEAN_PATH}/${i}_R1.fq.gz \
+                  -2 ${CLEAN_PATH}/${i}_R2.fq.gz \
+                  -S ${ALIGN_PATH}/${i}_${SPECIES}_hisat2.sam
+
+    ${SAMTOOL_PATH}/samtools sort \
+                   -@ ${CORENUM} \
+                   -o ${ALIGN_PATH}/${i}_${SPECIES}_hisat2.bam \
+                   ${ALIGN_PATH}/${i}_${SPECIES}_hisat2.sam
+
+    ${SAMTOOL_PATH}/samtools index ${ALIGN_PATH}/${i}_${SPECIES}_hisat2.bam
+
+    ${RM_PATH} ${ALIGN_PATH}/${i}_${SPECIES}_hisat2.sam
+    echo "====================================="
+
+done
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~1stadd virus~~~~~~~~~~~~~~~~~~~~~~~~~~
+fq=($(ls ${CLEAN_PATH} | grep .fq.gz))
+fqnames=($(echo "${fq[@]%_*}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
+
+for i in ${fqnames[@]}; do
+
+    echo "===================================="
+    echo "Kallisto using ${SPECIES} cDNA for ${i}."
+    ${KALLISTO_PATH}/kallisto quant \
+                    -t ${CORENUM} \
+                    -i ${REF_PATH}/ath_latentvirus1.kindex \
+                    -o ${ALIGN_PATH}/${i}_${SPECIES}_kallisto \
+                    ${CLEAN_PATH}/${i}_R1.fq.gz ${CLEAN_PATH}/${i}_R2.fq.gz
+
+    echo "HISAT2 using ${SPECIES} genome for ${i}."
+    ${HISAT2_PATH}/hisat2 -p ${CORENUM} \
+                  -x ${REF_PATH}/athlatenvirus1ht2index/genome \
                   -1 ${CLEAN_PATH}/${i}_R1.fq.gz \
                   -2 ${CLEAN_PATH}/${i}_R2.fq.gz \
                   -S ${ALIGN_PATH}/${i}_${SPECIES}_hisat2.sam
