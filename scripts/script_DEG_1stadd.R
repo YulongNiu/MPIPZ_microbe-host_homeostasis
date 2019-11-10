4fdd6bf8df160474b3c86bae1d2d58f5773b4747
Yulong Niu <yulong.niu@hotmail.com>
2019-11-10 15:53:10 +0100

    update PCA plots

--- scripts/script_DEG_1stadd.R
+++ scripts/script_DEG_1stadd.R
@@ -146,105 +146,6 @@ res <- cbind.data.frame(as.matrix(mcols(degres)[, 1:10]), assay(ntd), stringsAsF
 write_csv(res, 'eachGroup_vs_Mock_k_1stadd.csv')
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-
-##~~~~~~~~~~~~~~~~~~~~~~~~DEG flg22_SynCom vs flg22~~~~~~~~~~~~~~~~~
-setwd('/extDisk1/RESEARCH/MPIPZ_KaWai_RNASeq/results/')
-
-## sampleTable
-sampleTable <- data.frame(condition = factor(rep(c('Mock', 'Flg22', 'Flg22_SynCom33', 'Flg22_SynCom35'), each = 3)), process = factor(rep(c('N', 'Y', 'Y', 'Y'), each = 3)))
-sampleTable$condition %<>% relevel(ref = 'Flg22')
-rownames(sampleTable) <- colnames(kres$counts)
-
-degres <- DESeqDataSetFromTximport(kres,
-                                   sampleTable,
-                                   ~condition)
-
-## DEGs role out two zeros in one group
-## degres <- degres[rowSums(counts(degres)) > 1, ]
-degres %<>%
-  estimateSizeFactors %>%
-  counts(normalized = TRUE) %>%
-  apply(1, checkFlg22, 1) %>%
-  degres[., ]
-degres <- DESeq(degres)
-
-## count transformation
-rld <- rlog(degres)
-vst <- varianceStabilizingTransformation(degres)
-ntd <- normTransform(degres)
-
-cond <- degres %>%
-  resultsNames %>%
-  str_extract('(?<=condition_).*') %>%
-  .[!is.na(.)] %>%
-  `[`(1:2)
-
-resRaw <- lapply(cond,
-                 function(x) {
-                   degres %>%
-                     results(name = paste0('condition_', x)) %T>%
-                     summary %>%
-                     as_tibble %>%
-                     select(pvalue, padj, log2FoldChange) %>%
-                     rename_all(.funs = list(~paste0(x, '_', .)))
-                 }) %>%
-  bind_cols
-
-res <- cbind.data.frame(as.matrix(mcols(degres)[, 1:10]), assay(ntd), stringsAsFactors = FALSE) %>%
-  rownames_to_column(., var = 'ID') %>%
-  as_tibble %>%
-  bind_cols(resRaw) %>%
-  inner_join(anno, by = 'ID') %>%
-  select(ID, Gene : Description, Flg22_1 : Flg22_SynCom35_vs_Flg22_log2FoldChange) %>%
-  arrange(Flg22_SynCom33_vs_Flg22_padj)
-
-write_csv(res, 'SynCom_vs_flg22_k.csv')
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~SynCom35 vs. SynCom35~~~~~~~~~~~~~~~~~~
-setwd('/extDisk1/RESEARCH/MPIPZ_KaWai_RNASeq/results/')
-
-## sampleTable
-sampleTable <- data.frame(condition = factor(rep(c('Mock', 'Flg22', 'Flg22_SynCom33', 'Flg22_SynCom35'), each = 3)), process = factor(rep(c('N', 'Y', 'Y', 'Y'), each = 3)))
-sampleTable$condition %<>% relevel(ref = 'Flg22_SynCom33')
-rownames(sampleTable) <- colnames(kres$counts)
-
-degres <- DESeqDataSetFromTximport(kres,
-                                   sampleTable,
-                                   ~condition)
-
-## DEGs role out two zeros in one group
-## degres <- degres[rowSums(counts(degres)) > 1, ]
-degres %<>%
-  estimateSizeFactors %>%
-  counts(normalized = TRUE) %>%
-  apply(1, checkFlg22, 1) %>%
-  degres[., ]
-degres <- DESeq(degres)
-
-## count transformation
-rld <- rlog(degres)
-vst <- varianceStabilizingTransformation(degres)
-ntd <- normTransform(degres)
-
-resRaw <- degres %>%
-  results(name = 'condition_Flg22_SynCom35_vs_Flg22_SynCom33') %T>%
-  summary %>%
-  as_tibble %>%
-  select(pvalue, padj, log2FoldChange) %>%
-  rename_all(.funs = list(~paste0('Flg22_SynCom35_vs_Flg22_SynCom33_', .)))
-
-res <- cbind.data.frame(as.matrix(mcols(degres)[, 1:10]), assay(ntd), stringsAsFactors = FALSE) %>%
-  rownames_to_column(., var = 'ID') %>%
-  as_tibble %>%
-  bind_cols(resRaw) %>%
-  inner_join(anno, by = 'ID') %>%
-  select(ID, Gene : Description, Flg22_SynCom33_1 : Flg22_SynCom35_vs_Flg22_SynCom33_log2FoldChange) %>%
-  arrange(Flg22_SynCom35_vs_Flg22_SynCom33_padj)
-
-write_csv(res, 'SynCom35_vs_SynCom33_k_full.csv')
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~PCA~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 library('directlabels')
 library('ggplot2')
@@ -344,8 +245,8 @@ ggplot(pcaData, aes(x = PC1, y = PC2, colour = Group)) +
   ylab(paste0("PC2: ",percentVar[2],"% variance")) +
   geom_dl(aes(label = ID, color = Group), method = 'smart.grid') +
   scale_colour_manual(values = levels(cols)[colorIdx])
-ggsave('PCA_1stadd_like1st_C12.pdf', width = 15, height = 12)
-ggsave('PCA_1stadd_like1st_C12.jpg', width = 15, height = 12)
+ggsave('PCA_1stadd_like1st.pdf', width = 15, height = 12)
+ggsave('PCA_1stadd_like1st.jpg', width = 15, height = 12)
 
 
 rldDataPart <- rldData[, sampleIdx]
