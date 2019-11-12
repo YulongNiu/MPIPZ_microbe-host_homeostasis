bde01b765517650689e0b9aa59a616307af0677f
Yulong Niu <yulong.niu@hotmail.com>
2019-11-12 16:38:19 +0100

    update split PCA plot

--- scripts/script_DEG_1stadd.R
+++ scripts/script_DEG_1stadd.R
@@ -195,37 +195,43 @@ percentVar <- round(100 * percentVar)
 pca1 <- pca$x[,2]
 pca2 <- pca$x[,3]
 pcaData <- data.frame(PC1 = pca1, PC2 = pca2, Group = colData(rld)[, 1], ID = rownames(colData(rld)))
-ggplot(pcaData, aes(x = PC1, y = PC2, colour = Group)) +
+ggplot(pcaData, aes(x = PC1, y = PC2, colour = Group, label = ID)) +
   geom_point(size = 3) +
   xlab(paste0("PC2: ",percentVar[2],"% variance")) +
   ylab(paste0("PC3: ",percentVar[3],"% variance")) +
-  geom_dl(aes(label = ID, color = Group), method = 'smart.grid') +
-  scale_colour_manual(values = levels(cols))
+  scale_colour_manual(values = levels(cols)) +
+  geom_text_repel(force = 5)
 ggsave('PCA_1stadd_C23.pdf', width = 15, height = 12)
 ggsave('PCA_1stadd_C23.jpg', width = 15, height = 12)
 
 
 ## Mock + Mock_flg22
+prefixPCA <- 'M_M22'
 sampleIdx <- c(1:8)
 colorIdx <- c(1:2)
 
 ## 33
+prefixPCA <- 'M_M33'
 sampleIdx <- c(9:24)
 colorIdx <- c(3:6)
 
 ## 35
+prefixPCA <- 'M_M35'
 sampleIdx <- c(25:40)
 colorIdx <- c(7:10)
 
 ## flg22
+prefixPCA <- 'flg22'
 sampleIdx <- c(5:8, 13:16, 21:24, 29:32, 37:40)
 colorIdx <- c(2, 4, 6, 8, 10)
 
 ## 33 + 33_flg22 + 35 + 35_flg22
+prefixPCA <- '33_35'
 sampleIdx <- c(17:24, 33:40)
 colorIdx <- c(5:6, 9:10)
 
 ## mock + mock_flg22 + 33_flg22 + 35_flg22
+prefixPCA <- 'like1st'
 sampleIdx <- c(1:8, 21:24, 37:40)
 colorIdx <- c(1, 2, 6, 10)
 
@@ -236,14 +242,14 @@ percentVar <- round(100 * percentVar)
 pca1 <- pca$x[, 1]
 pca2 <- pca$x[, 2]
 pcaData <- data.frame(PC1 = pca1, PC2 = pca2, Group = colData(rld)[sampleIdx, 1], ID = rownames(colData(rld))[sampleIdx])
-ggplot(pcaData, aes(x = PC1, y = PC2, colour = Group)) +
+ggplot(pcaData, aes(x = PC1, y = PC2, colour = Group, label = ID)) +
   geom_point(size = 3) +
   xlab(paste0("PC1: ",percentVar[1],"% variance")) +
   ylab(paste0("PC2: ",percentVar[2],"% variance")) +
-  geom_dl(aes(label = ID, color = Group), method = 'smart.grid') +
-  scale_colour_manual(values = levels(cols)[colorIdx])
-ggsave('PCA_1stadd_35.pdf', width = 15, height = 12)
-ggsave('PCA_1stadd_35.jpg', width = 15, height = 12)
+  scale_colour_manual(values = levels(cols)[colorIdx]) +
+  geom_text_repel(force = 5)
+ggsave(paste0('PCA_1stadd_', prefixPCA, '.pdf'), width = 15, height = 12)
+ggsave(paste0('PCA_1stadd_', prefixPCA, '.jpg'), width = 15, height = 12)
 
 ## 3D plot
 library('rgl')
