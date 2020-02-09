196655faa3af666b7c904f0687bb4d343e11a1de
Yulong Niu <yulong.niu@hotmail.com>
2020-02-09 18:34:41 +0100

    re-organize data

--- scripts/script_DEG_soil.R
+++ scripts/script_DEG_soil.R
@@ -10,7 +10,7 @@ checkFlg22 <- function(v, threshold) {
   require('magrittr')
 
   res <- v %>%
-    split(rep(1 : 3, each = 4)) %>%
+    split(rep(1 : 3, each = 3)) %>%
     sapply(checkZeros, threshold) %>%
     all
 
@@ -18,15 +18,11 @@ checkFlg22 <- function(v, threshold) {
 }
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-
 library('tximport')
 library('rhdf5')
 library('magrittr')
 library('DESeq2')
-library('tibble')
-library('readr')
-library('dplyr')
-library('stringr')
+library('tidyverse')
 
 anno <- read_csv('/extDisk1/RESEARCH/MPIPZ_KaWai_RNASeq/results/Ensembl_ath_Anno.csv',
                  col_types = cols(Chromosome = col_character())) %>%
@@ -58,23 +54,59 @@ condi <- c('Mock', 'SynCom33', 'SynCom35')
 sampleTable <- data.frame(condition = factor(rep(condi, each = 3), levels = condi))
 rownames(sampleTable) <- colnames(kres$counts)
 
+sampleTable$condition %<>% relevel(ref = 'Mock')
+
 degres <- DESeqDataSetFromTximport(kres, sampleTable, ~condition)
 
-## DEGs
+## remove 0|0|x, 0|0|0
 degres %<>%
   estimateSizeFactors %>%
   counts(normalized = TRUE) %>%
   apply(1, checkFlg22, 1) %>%
   degres[., ]
-## degres <- degres[rowSums(counts(degres)) > 1, ]
-save(degres, file = 'degres_condi_Mock_soil.RData')
+
 degres <- DESeq(degres)
-## resultsNames(degres)
 
 ## count transformation
 rld <- rlog(degres)
-vst <- varianceStabilizingTransformation(degres)
 ntd <- normTransform(degres)
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~hidden batch effect~~~~~~~~~~~~~~~~~~~~~
+library('sva')
+library('ggplot2')
+
+dat <- rld %>%
+  assay %>%
+  {.[rowMeans(.) > 1, ]}
+mod <- model.matrix(~ condition, colData(degres))
+mod0 <- model.matrix(~ 1, colData(degres))
+
+## manual detect surrogate variance
+svnum <- 4
+svseq <- svaseq(dat, mod, mod0, n.sv = svnum)
+
+## auto detect sv
+svobj <- sva(dat, mod, mod0)
+svnum <- svobj$sv %>% ncol
+
+svobj$sv %>%
+  set_colnames(paste0('sv', seq_len(svnum))) %>%
+  as_tibble %>%
+  gather(key = 'sv', value = 'value') %>%
+  mutate(condition = colData(degres) %>%
+           .$condition %>%
+           rep(svnum) %>%
+           as.character,
+         sample = rep(colnames(degres), svnum)) %>%
+  mutate(group = paste(sv, condition, sep = '_')) %>%
+  ggplot(aes(sample, value, colour = sv, group = group)) +
+  geom_point() +
+  geom_line() +
+  theme(axis.text.x = element_text(angle = 90))
+ggsave('auto_sv_soil.jpg')
+ggsave('auto_sv_soil.pdf')
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 cond <- degres %>%
   resultsNames %>%
@@ -184,27 +216,43 @@ degres <- DESeq(degres)
 rld <- rlog(degres)
 vst <- varianceStabilizingTransformation(degres)
 ntd <- normTransform(degres)
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-resRaw <- degres %>%
-  results(name = 'condition_Flg22_SynCom35_vs_Flg22_SynCom33') %T>%
-  summary %>%
-  as_tibble %>%
-  select(pvalue, padj, log2FoldChange) %>%
-  rename_all(.funs = list(~paste0('Flg22_SynCom35_vs_Flg22_SynCom33_', .)))
+##~~~~~~~~~~~~~~~~~~~~~~~~DEG Mock vs. 3 conditions~~~~~~~~~~~~~~~~~
+degres$sv1 <- svobj$sv[, 1]
+degres$sv2 <- svobj$sv[, 2]
+design(degres) <- ~sv1 + sv2 + condition
 
-res <- cbind.data.frame(as.matrix(mcols(degres)[, 1:10]), assay(ntd), stringsAsFactors = FALSE) %>%
+degres <- DESeq(degres)
+
+cond <- list(c('SynCom33', 'Mock'),
+             c('SynCom35', 'Mock'),
+             c('SynCom33', 'SynCom35'))
+
+resRaw <- lapply(cond,
+                 function(x) {
+                   degres %>%
+                     results(contrast = c('condition', x)) %T>%
+                     summary %>%
+                     as_tibble %>%
+                     select(pvalue, padj, log2FoldChange) %>%
+                     rename_all(.funs = list(~paste0(paste(x, collapse = '_vs_'), '_', .)))
+                 }) %>%
+  bind_cols
+
+res <- cbind.data.frame(as.matrix(mcols(degres)[, 1:10]), counts(degres, normalize = TRUE), stringsAsFactors = FALSE) %>%
   rownames_to_column(., var = 'ID') %>%
   as_tibble %>%
   bind_cols(resRaw) %>%
   inner_join(anno, by = 'ID') %>%
-  select(ID, Gene : Description, Flg22_SynCom33_1 : Flg22_SynCom35_vs_Flg22_SynCom33_log2FoldChange) %>%
-  arrange(Flg22_SynCom35_vs_Flg22_SynCom33_padj)
+  select(ID, Gene : Description, Mock_1 : SynCom33_vs_SynCom35_log2FoldChange) %>%
+  arrange(SynCom33_vs_Mock_padj)
 
-write_csv(res, 'SynCom35_vs_SynCom33_k_full.csv')
-##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+write_csv(res, 'eachGroup_vs_Mock_k_1stadd.csv')
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~PCA~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-library('directlabels')
+library('ggrepel')
 library('ggplot2')
 library('RColorBrewer')
 library('limma')
@@ -212,130 +260,48 @@ library('sva')
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ## remove low count
-thres <- 0
-rldData <- assay(rld)
+dat <- rld %>%
+  assay %>%
+  {.[rowMeans(.) > 1, ]}
 
-rl <- apply(rldData, 1, function(x){
-  return(sum(x > thres) == length(x))
-})
-rldData %<>% .[rl, ]
-
-## batch correction limma
-rldData %<>% removeBatchEffect(rep(1 : 3, 3) %>% factor)
-
-## batch correction sva
-modcombat <- model.matrix(~1, data = sampleTable)
-rldData %<>% ComBat(dat = ., batch = rep(rep(1 : 3, 3) %>% factor) %>% factor, mod = modcombat, par.prior = TRUE, prior.plots = FALSE)
+group <- sampleTable$condition
+design <- model.matrix(~ group)
+rldData <- dat %>%
+  removeBatchEffect(covariates = svobj$sv,
+                    design = design)
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-cols <- colData(rld)[, 1] %>% factor(., labels = brewer.pal(10, name = 'Paired')[c(1, 5, 9)])
-
-## 1 - 2 C
 pca <- prcomp(t(rldData))
 percentVar <- pca$sdev^2/sum(pca$sdev^2)
 percentVar <- round(100 * percentVar)
 pca1 <- pca$x[,1]
 pca2 <- pca$x[,2]
-pcaData <- data.frame(PC1 = pca1, PC2 = pca2, Group = colData(rld)[, 1], ID = rownames(colData(rld)))
-ggplot(pcaData, aes(x = PC1, y = PC2, colour = Group)) +
-  geom_point(size = 3) +
+pcaData <- data.frame(PC1 = pca1, PC2 = pca2, Group = colData(rld)[, 1], ID = rownames(colData(rld))) %>%
+  mutate(SynCom = rep(c('Mock', 'Non-suppressive', 'Suppressive'), c(3, 3, 3))) %>%
+  mutate(Treatment = rep(c('Mock', 'SynCom'), c(3, 6)) %>% factor) %>%
+  mutate(flg22 = c('without') %>% rep(9) %>% factor)
+
+ggplot(pcaData, aes(x = PC1, y = PC2, colour = SynCom)) +
+  geom_point(aes(shape = Treatment), size = 4) +
   xlab(paste0("PC1: ",percentVar[1],"% variance")) +
   ylab(paste0("PC2: ",percentVar[2],"% variance")) +
-  geom_dl(aes(label = ID, color = Group), method = 'smart.grid') +
-  scale_colour_manual(values = levels(cols))
-ggsave('PCA_soil_SVA.pdf', width = 15, height = 12)
-ggsave('PCA_soil_SVA.jpg', width = 15, height = 12)
+  scale_colour_manual(values = c('#000000', '#377eb8', '#e41a1c')) +
+  scale_shape_manual(values = c(1, 15),
+                     name = 'Experimental\nConditions') +
+  stat_ellipse(aes(x = PC1, y = PC2, group = Group, linetype = flg22), type = 't', level = 0.7) +
+  scale_linetype_manual(values = c(1, 2), guide = FALSE) +
+  coord_fixed(1) +
+  theme_classic() +
+  theme(plot.title = element_text(hjust = 0.5, size = 12, face = 'bold'),
+        legend.text.align = 0,
+        axis.text = element_text(size = 13),
+        axis.title = element_text(size = 14),
+        legend.text=element_text(size= 13),
+        legend.title = element_text(size = 14))
 
-## 2 - 3 C
-pca <- prcomp(t(rldData))
-percentVar <- pca$sdev^2/sum(pca$sdev^2)
-percentVar <- round(100 * percentVar)
-pca1 <- pca$x[,2]
-pca2 <- pca$x[,3]
-pcaData <- data.frame(PC1 = pca1, PC2 = pca2, Group = colData(rld)[, 1], ID = rownames(colData(rld)))
-ggplot(pcaData, aes(x = PC1, y = PC2, colour = Group)) +
-  geom_point(size = 3) +
-  xlab(paste0("PC2: ",percentVar[2],"% variance")) +
-  ylab(paste0("PC3: ",percentVar[3],"% variance")) +
-  geom_dl(aes(label = ID, color = Group), method = 'smart.grid') +
-  scale_colour_manual(values = levels(cols))
-ggsave('PCA_soil.pdf', width = 15, height = 12)
-ggsave('PCA_soil.jpg', width = 15, height = 12)
+ggsave('PCA_soil_sva.pdf', width = 13)
+ggsave('PCA_soil_sva.jpg', width = 13)
 
-
-## Mock + Mock_flg22
-sampleIdx <- c(1:8)
-colorIdx <- c(1:2)
-
-## 33
-sampleIdx <- c(9:24)
-colorIdx <- c(3:6)
-
-## 35
-sampleIdx <- c(25:40)
-colorIdx <- c(7:10)
-
-## flg22
-sampleIdx <- c(5:8, 13:16, 21:24, 29:32, 37:40)
-colorIdx <- c(2, 4, 6, 8, 10)
-
-## 33 + 33_flg22 + 35 + 35_flg22
-sampleIdx <- c(17:24, 33:40)
-colorIdx <- c(5:6, 9:10)
-
-## mock + mock_flg22 + 33_flg22 + 35_flg22
-sampleIdx <- c(1:8, 21:24, 37:40)
-colorIdx <- c(1, 2, 6, 10)
-
-rldDataPart <- rldData[, sampleIdx]
-pca <- prcomp(t(rldDataPart))
-percentVar <- pca$sdev^2/sum(pca$sdev^2)
-percentVar <- round(100 * percentVar)
-pca1 <- pca$x[, 1]
-pca2 <- pca$x[, 2]
-pcaData <- data.frame(PC1 = pca1, PC2 = pca2, Group = colData(rld)[sampleIdx, 1], ID = rownames(colData(rld))[sampleIdx])
-ggplot(pcaData, aes(x = PC1, y = PC2, colour = Group)) +
-  geom_point(size = 3) +
-  xlab(paste0("PC1: ",percentVar[1],"% variance")) +
-  ylab(paste0("PC2: ",percentVar[2],"% variance")) +
-  geom_dl(aes(label = ID, color = Group), method = 'smart.grid') +
-  scale_colour_manual(values = levels(cols)[colorIdx])
-ggsave('PCA_1stadd_like1st_C12.pdf', width = 15, height = 12)
-ggsave('PCA_1stadd_like1st_C12.jpg', width = 15, height = 12)
-
-
-rldDataPart <- rldData[, sampleIdx]
-pca <- prcomp(t(rldDataPart))
-percentVar <- pca$sdev^2/sum(pca$sdev^2)
-percentVar <- round(100 * percentVar)
-pca1 <- pca$x[, 2]
-pca2 <- pca$x[, 3]
-pcaData <- data.frame(PC1 = pca1, PC2 = pca2, Group = colData(rld)[sampleIdx, 1], ID = rownames(colData(rld))[sampleIdx])
-ggplot(pcaData, aes(x = PC1, y = PC2, colour = Group)) +
-  geom_point(size = 3) +
-  xlab(paste0("PC2: ",percentVar[2],"% variance")) +
-  ylab(paste0("PC3: ",percentVar[3],"% variance")) +
-  geom_dl(aes(label = ID, color = Group), method = 'smart.grid') +
-  scale_colour_manual(values = levels(cols)[colorIdx])
-ggsave('PCA_1stadd_like1st_C23.pdf', width = 15, height = 12)
-ggsave('PCA_1stadd_like1st_C23.jpg', width = 15, height = 12)
-
-
-## 3D plot
-library('rgl')
-cols <- colData(rld)[, 1] %>% factor(., labels = brewer.pal(10, name = 'Paired'))
-plot3d(pca$x[, 1:3],
-       col = cols,
-       size = 10,
-       xlab = paste0('PC1: ',percentVar[1],'% variance'),
-       ylab = paste0('PC2: ',percentVar[2],'% variance'),
-       zlab = paste0('PC3: ',percentVar[3],'% variance'))
-
-dir.create('animation_merge')
-for (i in 1:360) {
-  view3d(userMatrix=rotationMatrix(2*pi * i/360, 0, 1, 0))
-  rgl.snapshot(filename=paste('animation_merge/frame-',
-                              sprintf('%03d', i), '.jpg', sep=''))}
-
+save(degres, rldData, file = 'degres_condi_Mock.RData')
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ##################################################################
