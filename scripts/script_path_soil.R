eb71968260d4917bfe5c6fbbb4cde840f856e16a
Yulong Niu <yulong.niu@hotmail.com>
2020-03-06 17:15:52 +0100

    add soil GO heamap

--- scripts/script_path_soil.R
+++ scripts/script_path_soil.R
@@ -419,3 +419,148 @@ for (i in cln) {
   }
 }
 ##################################################################
+
+##############################Plot GO heatmap##########################
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~useful funcs~~~~~~~~~~~~~~~~~~~~~~~~~~~
+meanFlg22 <- function(v) {
+
+  require('magrittr')
+
+  res <- v %>%
+    split(rep(1 : 3, each = 3)) %>%
+    sapply(mean, na.rm = TRUE)
+
+  return(res)
+}
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+library('tidyverse')
+library('DESeq2')
+library('ComplexHeatmap')
+library('RColorBrewer')
+library('circlize')
+
+savepath <- '/extDisk1/RESEARCH/MPIPZ_KaWai_RNASeq/results/removeZero/geneset_soil/clusterbc'
+setwd(savepath)
+
+load('kmeans10_soil_cp_BP.RData')
+
+topGONum <- 10
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Sig terms~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+anno <- read_csv('/extDisk1/RESEARCH/MPIPZ_KaWai_RNASeq/results/Ensembl_ath_Anno.csv',
+                 col_types = cols(Chromosome = col_character())) %>%
+  mutate_all(list(~replace(., is.na(.), ''))) %>%
+  mutate(GeneID = strsplit(ID, split = '.', fixed = TRUE) %>%
+           sapply('[[', 1) %>%
+           unlist) %>%
+  mutate(Gene = if_else(nchar(Gene) == 0, GeneID, Gene)) %>%
+  dplyr::select(GeneID, Gene, Description) %>%
+  dplyr::slice(which(!duplicated(.)))
+
+cpBP <- clusterProfiler:::fortify.compareClusterResult(kallGOBP,
+                                                       showCategory = topGONum) %>%
+  as_tibble %>%
+  mutate(geneName = sapply(geneID, function(x) {
+    strsplit(x, split = '/', fixed = TRUE) %>%
+      unlist %>%
+      tibble(GeneID = .) %>%
+      inner_join(anno) %>%
+      .$Gene %>%
+      paste(collapse = '/') %>%
+      str_replace('C/VIF2', 'C-VIF2') ## replace genes with '/'
+  }))
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~heatmap matrix~~~~~~~~~~~~~~~~~~~~~~
+load('/extDisk1/RESEARCH/MPIPZ_KaWai_RNASeq/results/removeZero/degres_condi_Mock_soil.RData')
+
+wholeDEG <- read_csv('/extDisk1/RESEARCH/MPIPZ_KaWai_RNASeq/results/removeZero/eachGroup_vs_Mock_k_soil.csv')
+kmeansRes <- read_csv('/extDisk1/RESEARCH/MPIPZ_KaWai_RNASeq/results/removeZero/kmeans10_soil.csv') %>%
+  select(ID, cl)
+
+fcsig <- wholeDEG %>%
+  select(ends_with('FoldChange')) %>%
+  transmute_all(list(~ case_when(. > log2(1.5) ~ 1,
+                                 . < -log2(1.5) ~ -1,
+                                 TRUE ~ 0)))
+padjsig <- wholeDEG %>%
+  select(ends_with('padj')) %>%
+  `<`(0.05) %>%
+  as_tibble %>%
+  transmute_all(list(~ if_else(is.na(.), FALSE, .)))
+
+heatsig <- (padjsig * fcsig) %>%
+  as_tibble %>%
+  abs %>%
+  rowSums %>%
+  {. >= 1} %>%
+  which %>%
+  dplyr::slice(wholeDEG, .) %>%
+  inner_join(kmeansRes)
+
+rawC <- rldData %>%
+  as.data.frame %>%
+  rownames_to_column('ID') %>%
+  as_tibble %>%
+  inner_join(heatsig %>% select(ID, cl))
+## inner_join(kmeansRes) ## all transcripts
+
+## scale counts
+scaleC <- rawC %>%
+  select(contains('_')) %>%
+  t %>%
+  scale %>%
+  t %>%
+  as_tibble %>%
+  bind_cols(rawC %>% select(ID, cl)) %>%
+  mutate(GeneID = ID %>% strsplit(split = '.', fixed = TRUE) %>% sapply('[', 1))
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+## top 5
+interesGO <- list(hypoxia = c('GO:0001666', 'GO:0036293', 'GO:0071456', 'GO:0071453', 'GO:0036294'),
+                  defense = c('GO:0071236', 'GO:0009627', 'GO:0009863', 'GO:0009751', 'GO:0071446', 'GO:0035690', 'GO:0042537'),
+                  cell_wall = c('GO:0042546', 'GO:0010413', 'GO:0045492', 'GO:0045491'))
+
+## top 10
+interesGO <- list(hypoxia = c('GO:0001666', 'GO:0036293', 'GO:0070482', 'GO:0071456', 'GO:0071453', 'GO:0036294'),
+                  defense = c('GO:0071236', 'GO:0009627', 'GO:0009863', 'GO:0009751', 'GO:0071446', 'GO:0071407', 'GO:0035690', 'GO:0042537', 'GO:0071395', 'GO:0009863', 'GO:0009867'),
+                  cell_wall = c('GO:0044038', 'GO:0010410', 'GO:0042546', 'GO:0070592', 'GO:0010413', 'GO:0045492', 'GO:0045491', 'GO:0070589'))
+
+
+for (i in seq_along(interesGO)) {
+
+  interesGene <- cpBP %>%
+    filter(ID %in% interesGO[[i]]) %>%
+    .$geneID %>%
+    strsplit(split = '/', fixed = TRUE) %>%
+    unlist %>%
+    unique
+
+  interesMat <- scaleC %>%
+    dplyr::filter(GeneID %in% interesGene) %>%
+    dplyr::filter(!(cl %in% c(6)))
+
+  matcol <- colorRamp2(seq(min(scaleC %>% select(contains('_'))), max(scaleC %>% select(contains('_'))), length = 100), colorRampPalette(rev(brewer.pal(n = 10, name = 'Spectral'))[c(-3, -4, -6, -7)])(100))
+
+  dim(interesMat) %>% print
+
+  ht_list <- Heatmap(matrix = interesMat %>%
+                       select(contains('_')) %>%
+                       apply(1, meanFlg22) %>%
+                       t,
+                     name = 'Scaled Counts',
+                     row_order = order(interesMat$cl) %>% rev,
+                     row_split = interesMat$cl,
+                     row_gap = unit(2, "mm"),
+                     column_order = 1 : 3,
+                     column_split = rep(c('Mock', 'Non-sup', 'Sup'), c(1, 1, 1)),
+                     show_column_names = FALSE,
+                     col = matcol,
+                     use_raster = FALSE)
+
+  pdf(paste0(savepath, '/', 'GO_', names(interesGO)[i], '_soil_', topGONum, '.pdf'))
+  draw(ht_list)
+  dev.off()
+}
+######################################################################
