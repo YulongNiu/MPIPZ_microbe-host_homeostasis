eb5cd42fc95c82c203edcabaadc75e6d6ddf91b5
Yulong Niu <yulong.niu@hotmail.com>
2020-04-27 10:36:31 +0200

    update comparative and other updates

--- scripts/script_replot_heatmap_combine.R
+++ scripts/script_replot_heatmap_combine.R
@@ -4,8 +4,16 @@ library('ComplexHeatmap')
 library('limma')
 library('DESeq2')
 library('RColorBrewer')
+library('foreach')
+library('magrittr')
+library('org.At.tair.db')
+library('clusterProfiler')
 
-setwd('/extDisk1/RESEARCH/MPIPZ_KaWai_RNASeq/results/removeZero')
+basepath <- '/run/media/Yulong/MyPassport/backup_20200320/'
+
+basepath %>%
+  file.path('MPIPZ_KaWai_RNASeq/results/removeZero') %>%
+  setwd
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~load WER and Col0 data~~~~~~~~~~~~~~~~~~~
 load('degres_condi_Mock.RData')
@@ -70,18 +78,6 @@ sigMat <- (padjsig * fcsig) %>%
                                 . == 1 ~ 'up'))) %>%
   mutate(ID = scaleCCol0$ID)
 
-sigMat <- (padjsig * fcsig) %>%
-  as_tibble %>%
-  setNames(names(.) %>% substr(., start = 1, stop = nchar(.) - 5)) %>%
-  mutate(ID = wholeDEG$ID) %>%
-  inner_join(scaleCCol0 %>% select(ID), .) %T>%
-  {(sum(.$ID == scaleCCol0$ID) == nrow(.)) %>% print} %>%
-  transmute_at(.var = vars(contains('vs')),
-               list(~ case_when(. == -1 ~ 'down',
-                                . == 0 ~'no',
-                                . == 1 ~ 'up'))) %>%
-  mutate(ID = scaleCCol0$ID)
-
 ## flg22 matrix
 flg22Col <- sigMat %>% select(1:5, ID)
 colnames(flg22Col)[1:5] <- c('Mock+flg22 vs. Mock',
@@ -100,7 +96,9 @@ colnames(hkCol)[1:2] <- c('SynCom33 vs. HKSynCom33',
 ##   select(ID, cl) %>%
 ##   filter(cl %in% c(10, 3))
 
-ironHKLive <- read_csv('/extDisk1/RESEARCH/MPIPZ_CJ_RNASeq/results/eachGroup_mergeDay8_deg_sig.csv')
+ironHKLive <- basepath %>%
+  file.path('MPIPZ_CJ_RNASeq/results/eachGroup_mergeDay8_deg_sig.csv') %>%
+  read_csv
 
 fcsigIron <- ironHKLive %>%
   select(ends_with('FoldChange')) %>%
@@ -122,29 +120,46 @@ sigMatIron <- (padjsigIron * fcsigIron) %>%
                                 . == 1 ~ 'bacup'))) %>%
   mutate(ID = ironHKLive$ID)
 
-ironRespSig <- read_csv('/extDisk1/RESEARCH/MPIPZ_CJ_RNASeq/results/eachGroup_mergeDay8_deg_sig.csv') %>%
+ironRespSig <- basepath %>%
+  file.path('MPIPZ_CJ_RNASeq/results/eachGroup_mergeDay8_deg_sig.csv') %>%
+  read_csv %>%
   select(ID, cl) %>%
   filter(cl %in% c(4, 8))
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~Castrillo flg22 longtime~~~~~~~~~~~~~~~~
-CastrilloSig <- read_csv('/extDisk1/RESEARCH/MPIPZ_KaWai_RNASeq/flg22_crossref/Castrillo_2017/DEGsflg22_singleend.csv') %>%
+CastrilloSig <- basepath %>%
+  file.path('MPIPZ_KaWai_RNASeq/flg22_crossref/Castrillo_2017/DEGsflg22_singleend.csv') %>%
+  read_csv %>%
   select(ID, flg22_vs_Col0_log2FoldChange) %>%
   dplyr::rename(Castrillo_log2FC = flg22_vs_Col0_log2FoldChange)
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~Volz flg22 shorttime~~~~~~~~~~~~~~~~
-VolzSig <- read_csv('/extDisk1/RESEARCH/MPIPZ_KaWai_RNASeq/flg22_crossref/Volz_2019/DEGs_pairend.csv') %>%
+VolzSig <- basepath %>%
+  file.path('MPIPZ_KaWai_RNASeq/flg22_crossref/Volz_2019/DEGs_pairend.csv') %>%
+  read_csv %>%
   select(ID, Col0_vs_flg22_log2FoldChange) %>%
   dplyr::rename(Volz_log2FC = Col0_vs_flg22_log2FoldChange) %>%
   mutate(Volz_log2FC = -Volz_log2FC)
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~Paulo flg22~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+PauloSig <- read_delim('Paulo_RNASeq_flg22.txt', delim = '\t')
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~Paulo HK vs living~~~~~~~~~~~~~~~~~~~~~~~
+PauloHKSig <- read_csv('Paulo_RNASeq_hklive_noflg22.csv') %>%
+  dplyr::filter(abs(Fold) > 1.5, FDR < 0.05) %>%
+  dplyr::select(Gene, logFC)
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~combine heatmap~~~~~~~~~~~~~~~~~~~~~~~~~~~
 sigCol0 <- read_csv('kmeans10_1stadd_sig.csv') %>%
   select(ID) %>%
   inner_join(scaleCCol0 %>% select(ID)) %>%
-  inner_join(scaleCWER %>% select(ID))
+  inner_join(scaleCWER %>% select(ID)) %>%
+  mutate(Gene = ID %>% substring(first = 1, last = nchar(.) - 2))
 
 scaleCCol0sig <- scaleCCol0 %>%
   inner_join(sigCol0)
@@ -164,11 +179,11 @@ ironHKLiveSig <- sigCol0 %>%
   inner_join(scaleCCol0sig %>% select(ID), .)
 
 ironCol0Sig <- sigCol0 %>%
-  left_join(., ironBacSig) %>%
-  dplyr::rename(ironbac = cl) %>%
-  mutate(ironbac = case_when(ironbac == 10 ~ 'bacup',
-                             ironbac == 3 ~ 'bacdown',
-                             is.na(ironbac) ~ 'bacno')) %>%
+  ## left_join(., ironBacSig) %>%
+  ## dplyr::rename(ironbac = cl) %>%
+  ## mutate(ironbac = case_when(ironbac == 10 ~ 'bacup',
+  ##                            ironbac == 3 ~ 'bacdown',
+  ##                            is.na(ironbac) ~ 'bacno')) %>%
   left_join(., ironRespSig) %>%
   dplyr::rename(ironresp = cl) %>%
   mutate(ironresp = case_when(ironresp == 8 ~ 'respup',
@@ -190,6 +205,18 @@ VolzCol0Sig <- sigCol0 %>%
                                       is.na(Volz_log2FC) ~ 'no')) %>%
   inner_join(scaleCCol0sig %>% select(ID), .)
 
+PauloCol0Sig <- sigCol0 %>%
+  left_join(., PauloSig) %>%
+  left_join(., PauloHKSig) %>%
+  mutate(Paulo_flg22 = case_when(Cluster %in% 1:4 ~ 'up',
+                                 Cluster %in% 5:8 ~ 'down',
+                                 is.na(Cluster) ~ 'no')) %>%
+  mutate(Paulo_bacresp = case_when(logFC > 0 ~ 'up',
+                                   logFC < 0 ~ 'down',
+                                   is.na(logFC) ~ 'no')) %>%
+  select(-Cluster, -logFC) %>%
+  inner_join(scaleCCol0sig %>% select(ID), .)
+
 c(sum(scaleCCol0sig$ID == scaleCWERSig$ID),
   sum(scaleCCol0sig$ID == flg22Col0Sig$ID),
   sum(scaleCCol0sig$ID == hkCol0Sig$ID),
@@ -197,11 +224,12 @@ c(sum(scaleCCol0sig$ID == scaleCWERSig$ID),
   ## sum(scaleCCol0sig$ID == ironCol0Sig$ID),
   sum(scaleCCol0sig$ID == CastrilloCol0Sig$ID),
   sum(scaleCCol0sig$ID == VolzCol0Sig$ID),
+  sum(scaleCCol0sig$ID == PauloCol0Sig$ID),
   nrow(scaleCCol0sig))
 
 ht_list <- Heatmap(matrix = scaleCCol0sig %>% select(contains('_')),
         name = 'Scaled Counts',
-        ## row_order = order(scaleCCol0sig$cl) %>% rev,
+        row_order = order(scaleCCol0sig$cl) %>% rev,
         row_split = scaleCCol0sig$cl,
         row_gap = unit(2, "mm"),
         column_order = 1 : 40,
@@ -218,19 +246,25 @@ ht_list <- Heatmap(matrix = scaleCCol0sig %>% select(contains('_')),
           col = colorRampPalette(rev(brewer.pal(n = 10, name = 'Spectral'))[c(-3, -4, -6, -7)])(100),
           column_title_gp = gpar(fontsize = 6),
           use_raster = FALSE) +
-  Heatmap(flg22Col0Sig %>% select(-ID),
+  Heatmap(flg22Col0Sig %>% select(-ID, -Gene),
           col = c('down' = 'blue', 'no' = 'white', 'up' = 'red'),
           column_names_gp = gpar(fontsize = 5),
           heatmap_legend_param = list(title = 'Col0flg22'),
           cluster_columns = FALSE,
           use_raster = FALSE) +
-  Heatmap(CastrilloCol0Sig %>% select(-ID),
+  Heatmap(PauloCol0Sig %>% select(-ID, -Gene, -Paulo_bacresp),
+          col = c('down' = 'blue', 'no' = 'white', 'up' = 'red'),
+          column_names_gp = gpar(fontsize = 5),
+          heatmap_legend_param = list(title = 'Paulo'),
+          cluster_columns = FALSE,
+          use_raster = FALSE) +
+  Heatmap(CastrilloCol0Sig %>% select(-ID, -Gene),
           col = c('down' = 'blue', 'no' = 'white', 'up' = 'red'),
           column_names_gp = gpar(fontsize = 5),
           heatmap_legend_param = list(title = 'Castrillo'),
           cluster_columns = FALSE,
           use_raster = FALSE) +
-  Heatmap(VolzCol0Sig %>% select(-ID),
+  Heatmap(VolzCol0Sig %>% select(-ID, -Gene),
           col = c('down' = 'blue', 'no' = 'white', 'up' = 'red'),
           column_names_gp = gpar(fontsize = 5),
           heatmap_legend_param = list(title = 'Volz'),
@@ -242,16 +276,22 @@ ht_list <- Heatmap(matrix = scaleCCol0sig %>% select(contains('_')),
   ##         heatmap_legend_param = list(title = 'IronBac'),
   ##         cluster_columns = FALSE,
   ##         use_raster = FALSE) +
-  Heatmap(ironHKLiveSig %>% select(-2:-5, -ID),
-          col = c('bacup' = 'blue', 'bacno' = 'white', 'bacdown' = 'red'),
-          column_names_gp = gpar(fontsize = 5),
-          heatmap_legend_param = list(title = 'IronBac'),
-          cluster_columns = FALSE,
-          use_raster = FALSE) +
-  Heatmap(hkCol0Sig %>% select(-ID),
-          col = c('up' = 'blue', 'no' = 'white', 'down' = 'red'),
+  Heatmap(hkCol0Sig %>% select(-ID, -Gene),
+          col = c('up' = 'red', 'no' = 'white', 'down' = 'blue'),
           column_names_gp = gpar(fontsize = 5),
           heatmap_legend_param = list(title = 'Col0HKlive'),
+          cluster_columns = FALSE,
+          use_raster = FALSE) +
+  Heatmap(PauloCol0Sig %>% select(-ID, -Gene, -Paulo_flg22),
+          col = c('down' = 'blue', 'no' = 'white', 'up' = 'red'),
+          column_names_gp = gpar(fontsize = 5),
+          heatmap_legend_param = list(title = 'PauloHKlive'),
+          cluster_columns = FALSE,
+          use_raster = FALSE) +
+  Heatmap(ironHKLiveSig %>% select(-2:-5, -ID, -Gene),
+          col = c('bacup' = 'red', 'bacno' = 'white', 'bacdown' = 'blue'),
+          column_names_gp = gpar(fontsize = 5),
+          heatmap_legend_param = list(title = 'IronBac'),
           cluster_columns = FALSE,
           use_raster = FALSE)
   ## Heatmap(ironCol0Sig %>% select(ironresp),
@@ -274,65 +314,118 @@ system(paste0('convert -density 1200 ', paste0(filePrefix, '.pdf'), ' ', paste0(
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~HK vs live~~~~~~~~~~~~~~~~~~~~~~~
-## library('venn')
-## library('VennDiagram')
-## library('venneuler')
 library('eulerr')
+library('VennDiagram')
 
 hkCol0SigVenn <- hkCol0Sig %>%
-  mutate_at(vars(-ID), list(~str_detect(., 'down|up'))) %>%
+  mutate_at(vars(-ID, -Gene), list(~str_detect(., 'down|up'))) %>%
   dplyr::rename(Nonsupp = `SynCom33 vs. HKSynCom33`,
                 Supp = `SynCom35 vs. HKSynCom35`)
 
 ironHKLiveVenn <- ironHKLiveSig %>%
-  select(-1:-5) %>%
+  select(-1:-6) %>%
   transmute(Iron = apply(., 1, function(x) {str_detect(x, 'bacdown|bacup') %>% any})) %>%
   mutate(ID = ironHKLiveSig$ID)
 
+PauloHKLiveVenn <- PauloCol0Sig %>%
+  mutate(Paulo = str_detect(Paulo_bacresp, 'down|up')) %>%
+  select(ID, Paulo)
+
 mergeVenn <- inner_join(hkCol0SigVenn, ironHKLiveVenn) %>%
+  inner_join(PauloHKLiveVenn) %>%
   inner_join(scaleCCol0sig %>% select(ID, cl))
 
-for (i in 1:10) {
-  pdf(paste0('iron_venn/cluster', i, '.pdf'))
-  mergeVenn %>%
-    ## filter(cl == i) %>%
-    dplyr::select(Iron, Nonsupp, Supp) %>%
+## output venn
+allVenn <- foreach (i = 1:10, .combine = inner_join) %do% {
+
+  eachVenn <- mergeVenn %>%
+    filter(cl == i) %>%
+    dplyr::select(Iron, Nonsupp, Supp, Paulo) %>%
     euler %>%
-    plot(quantities = TRUE,
-         labels = list(font = 4),
-         fill = c('#7CAE00', '#F1696D', '#21BDC3'))
-  dev.off()
+    .$original.values %>%
+    as.data.frame %>%
+    rownames_to_column('ID') %>%
+    magrittr::set_colnames(c('ID', paste0('cluster', i)))
 }
 
-## mergeVenn %>%
-##   filter(cl == 1) %>%
-##   dplyr::select(Iron, Nonsupp, Supp) %>%
-##   euler %>%
-##   .$original.values %>%
-##   venneuler %>%
-##   plot
+allVenn %<>%
+  mutate(clusterAll = allVenn %>% select(-ID) %>% rowSums)
+write_csv(allVenn, 'hk_living_veen.csv')
+
+## plot
+foreach (i = 1:10) %do% {
+
+  ## pdf(paste0('iron_venn/cluster', i, '.pdf'))
+
+  ## mergeVenn %>%
+  ##   filter(cl == i) %>%
+  ##   dplyr::select(Iron, Nonsupp, Supp, Paulo) %>%
+  ##   venneuler %>%
+  ##   plot
+
+  ## mergeVenn %>%
+  ##   filter(cl == i) %>%
+  ##   dplyr::select(Iron, Nonsupp, Supp, Paulo) %>%
+  ##   euler %>%
+  ##   plot(quantities = TRUE,
+  ##        labels = list(font = 4),
+  ##        fill = c('#ffff33', '#F1696D', '#21BDC3', '#7CAE00'))
+
+  mergeVenn %>%
+    filter(cl == i) %>%
+    dplyr::select(Iron, Nonsupp, Supp, Paulo) %>% {
+      vennList <- list(Iron = which(.$Iron),
+                       Nonsupp = which(.$Nonsupp),
+                       Supp = which(.$Supp),
+                       Paulo = which(.$Paulo))
+      return(vennList)} %>%
+    venn.diagram(filename = paste0('iron_venn2/cluster', i, '.pdf'),
+                 lwd = 2,
+                 lty = 'blank',
+                 fill = c('#ffff33', '#F1696D', '#21BDC3', '#7CAE00'),
+                 cex = .6,
+                 fontface = 'bold',
+                 fontfamily = 'sans',
+                 cat.cex = 0.6,
+                 cat.fontface = 'bold',
+                 cat.default.pos = 'outer',
+                 cat.fontfamily = 'sans')
+
+  ## dev.off()
+}
+
+## common GO
+comVeen <- mergeVenn %>%
+  filter(Nonsupp, Supp, Iron, Paulo) %>%
+  compareCluster(geneCluster = .,
+                 fun = 'enrichGO',
+                 OrgDb = 'org.At.tair.db',
+                 keyType= 'TAIR',
+                 ont = 'BP',
+                 universe = keys(org.At.tair.db),
+                 pAdjustMethod = 'BH',
+                 pvalueCutoff=0.01,
+                 qvalueCutoff=0.1)
+
+dotplot(comVeen, showCategory = 30)
+ggsave('common_HKvsLiving.pdf')
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~flg22 Venn~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+library('eulerr')
+library('magrittr')
 
 
-## list(Iron = eachVenn$iron %>% which,
-##      Nonsupp = eachVenn$Nonsupp %>% which,
-##      Supp = eachVenn$Supp %>% which) %>%
-##   venn.diagram(.,
-##                euler.d = TRUE,
-##                filename = 'tmp1.pdf')
-## venn(.,
-##    snames = c('Iron', 'Nonsupp', 'Supp'),
-##    ilab = TRUE,
-##    zcolor = 'style',
-##    size = 25,
-##    cexil = 1.2,
-##    cexsn = 1.5)
-
-
-
-
-
-
-
+pdf('flg22_response_DEGs.pdf')
+flg22Col %>% transmute_at(.var = vars(-ID),
+                          list(~ case_when(str_detect(., 'down|up') ~ TRUE,
+                                           str_detect(., 'no') ~ FALSE))) %>%
+  set_colnames(c('Mock+flg22 vs. Mock', 'HK_NS3+flg22 vs. HK_NS3', 'HK_S3+flg22 vs. HK_S3', 'NS3+flg22 vs. NS3', 'S3+flg22 vs. S3')) %>%
+  euler %>%
+  plot(quantities = TRUE,
+       labels = list(font = 4),
+       fill = c(NA, '#d9d9d9', '#d9d9d9', '#F1696D'))
+dev.off()
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #######################################################################
 
@@ -342,6 +435,7 @@ library('ComplexHeatmap')
 library('limma')
 library('DESeq2')
 library('RColorBrewer')
+library('clusterProfiler')
 
 setwd('/extDisk1/RESEARCH/MPIPZ_KaWai_RNASeq/results/removeZero')
 
@@ -418,3 +512,123 @@ dev.off()
 system(paste0('convert -density 1200 ', paste0(filePrefix, '.pdf'), ' ', paste0(filePrefix, '.jpg')))
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #######################################################################
+
+
+###########################compare Paulo##############################
+library('tidyverse')
+library('magrittr')
+library('foreach')
+library('corrplot')
+library('RColorBrewer')
+library('org.At.tair.db')
+library('clusterProfiler')
+
+basepath <- '/run/media/Yulong/MyPassport/backup_20200320/'
+
+basepath %>%
+  file.path('MPIPZ_KaWai_RNASeq/results/removeZero') %>%
+  setwd
+
+kmeansResSig <- read_csv('kmeans10_1stadd_sig.csv') %>%
+  dplyr::select(ID, cl)
+
+Paulo <- read_delim('Paulo_RNASeq_flg22.txt', delim = '\t')
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~Jaccard similarity~~~~~~~~~~~~~~~~~~~~~~~
+Col0PauloJS <- foreach (i = 1:10, .combine = rbind) %do% {
+
+  Col0Each <- kmeansResSig %>%
+    filter(cl == i) %>%
+    .$ID %>%
+    substr(start = 1, stop = nchar(.) - 2) %>%
+    unique
+
+  JacSim <- foreach(j = 1:8, .combine = c)  %do% {
+    PauloEach <- Paulo %>%
+      filter(Cluster == j) %>%
+      .$Gene %>%
+      unique
+
+    eachJacSim <- length(intersect(Col0Each, PauloEach)) / length(union(Col0Each, PauloEach))
+
+    return(eachJacSim)
+
+  } %>%
+  set_names(paste0('Paulo', 1:8))
+
+  return(JacSim)
+} %>%
+  set_rownames(paste0('Ka-Wai', 1:10)) %>%
+  round(digits = 3)
+
+Col0PauloJS <- apply(Col0PauloJS, 1:2, function(x){ifelse(x < 0.0001, 0, x)})
+
+pdf('KaWai_Paulo_JacSim.pdf')
+cols <- rev(brewer.pal(n = 11, name = 'RdBu'))[c(-3, -4, -5, -7, -8, -9)]
+colorRampPalette(cols)(50) %>%
+   corrplot(Col0PauloJS, col = ., cl.lim = c(0, 0.5))
+dev.off()
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~~cluster~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Col0List <- foreach (i = 1:10) %do% {
+
+  kmeansResSig %>%
+    filter(cl == i) %>%
+    .$ID %>%
+    substr(start = 1, stop = nchar(.) - 2) %>%
+    unique
+
+} %>%
+  set_names(paste0('Ka-Wai', 1:10))
+
+PauloList <- split(Paulo$Gene, Paulo$Cluster) %>%
+  lapply(unique) %>%
+  set_names(paste0('Paulo', 1:8))
+
+Col0PauloList <- list(
+  `Ka-Wai1-Paulo1` = c(1, 1),
+  `Ka-Wai1-Paulo2` = c(1, 2),
+  `Ka-Wai1-Paulo4` = c(1, 4),
+  `Ka-Wai2-Paulo5` = c(2, 5),
+  `Ka-Wai2-Paulo8` = c(2, 8),
+  `Ka-Wai3-Paulo1` = c(3, 1),
+  `Ka-Wai5-Paulo6` = c(5, 6),
+  `Ka-Wai5-Paulo8` = c(5, 8)) %>%
+  lapply(function(x) {
+    intersect(Col0List[[x[1]]], PauloList[[x[2]]])
+  })
+
+resList <- c(Col0List[1],
+             PauloList[c(1, 2, 4)],
+             Col0PauloList[1:3],
+             Col0List[2],
+             PauloList[c(5, 8)],
+             Col0PauloList[4:5],
+             Col0List[3],
+             PauloList[1],
+             Col0PauloList[6],
+             Col0List[5],
+             PauloList[c(6, 8)],
+             Col0PauloList[7:8])
+
+1:7
+8:12
+13:15
+16:20
+goBP <- compareCluster(geneCluster = resList[15],
+                       fun = 'enrichGO',
+                       OrgDb = 'org.At.tair.db',
+                       keyType= 'TAIR',
+                       ont = 'BP',
+                       universe = keys(org.At.tair.db),
+                       pAdjustMethod = 'BH',
+                       pvalueCutoff=0.01,
+                       qvalueCutoff=0.1)
+
+dotplot(goBP, showCategory = 20)
+ggsave('compare_Ka-Wai_Paulo_4.pdf', width = 15, height = 12)
+
+write_csv(as.data.frame(goBP), 'compare_Ka-Wai3_Paulo1.csv')
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+######################################################################
